2783e36bde9c79cf6cb3851d258ff9853936d20e
Łukasz Winowicz <lwinowicz@gmail.com>
2024-07-28 18:37:55 +0200

    added new wpf project

--- WpfDashboardApp/obj/Debug/net8.0-windows/WpfDashboardApp.AssemblyInfo.cs
+++ WpfDashboardApp/obj/Debug/net8.0-windows/WpfDashboardApp.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WpfDashboardApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b13d000f179bcdb69154fe5458fb6d7952dce97")]
-[assembly: System.Reflection.AssemblyProductAttribute("Server")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Server")]
+[assembly: System.Reflection.AssemblyProductAttribute("WpfDashboardApp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WpfDashboardApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
