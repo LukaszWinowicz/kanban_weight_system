bba91e7e68ac99dbcd9a8d3ff039ca61c078a2b4
Łukasz Winowicz <lwinowicz@gmail.com>
2024-08-26 16:38:57 +0200

    update designe of scale view table

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23df4f759f8ec43bd1cb496b70ed7fe5f8d7cd00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6048872c0bc0b964daa4755b32748481d47a6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
