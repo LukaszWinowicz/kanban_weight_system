aa6289c2c691bf97b384923216c34c8a87cc98f7
Łukasz Winowicz <lwinowicz@gmail.com>
2024-09-04 18:22:57 +0200

    added to BlazorApp new PropertyColumn with SingleItemWeight and Value

--- ApiServer/ApiServer.WindowsForms/obj/Debug/net8.0-windows/ApiServer.WindowsForms.AssemblyInfo.cs
+++ ApiServer/ApiServer.WindowsForms/obj/Debug/net8.0-windows/ApiServer.WindowsForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiServer.WindowsForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+617013c8316c029ee66863261ca3604d064f7c30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ff7355048e92ba32d13f4526bd5736abfce5ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiServer.WindowsForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiServer.WindowsForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BlazorApp/Models/ScaleReadingDto.cs
+++ BlazorApp/Models/ScaleReadingDto.cs
@@ -4,12 +4,19 @@ namespace BlazorApp.Models
 {
     public class ScaleReadingDto
     {
+        //public string ScaleName { get; set; }
+        //public string ItemName { get; set; }
+        //public decimal SingleItemWeight { get; set; }
+        //public bool IsConnected { get; set; }
+        //public ReadingDto LatestReading { get; set; }
+
+        //public decimal? Quantity { get; set; }
         public string ScaleName { get; set; }
         public string ItemName { get; set; }
         public decimal SingleItemWeight { get; set; }
+        public decimal Value { get; set; }
         public bool IsConnected { get; set; }
-        public ReadingDto LatestReading { get; set; }
-
         public decimal? Quantity { get; set; }
+        public DateTime? ReadingDate { get; set; }
     }
 }
