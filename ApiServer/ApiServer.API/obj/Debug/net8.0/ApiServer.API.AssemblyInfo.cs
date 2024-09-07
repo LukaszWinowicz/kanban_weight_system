f94253de1063d89d1be618a96953bebd6701ac63
Łukasz Winowicz <lwinowicz@gmail.com>
2024-09-07 07:26:57 +0200

    - added to NavMenu - FluentNavLink to Settings View - move GetMessageBarIntent to folder Helpers as static method

--- ApiServer/ApiServer.API/obj/Debug/net8.0/ApiServer.API.AssemblyInfo.cs
+++ ApiServer/ApiServer.API/obj/Debug/net8.0/ApiServer.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiServer.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9af9b7985ce0d2d1e91484b2b82f4cb6d8b5bcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+791054da4840bd7b65f9e65a751d1bc0e89f663e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiServer.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiServer.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ BlazorApp/Helpers/MessageIntentHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace BlazorApp.Helpers
+{
+    public class MessageIntentHelper
+    {
+        public static MessageIntent GetMessageBarIntent(bool isLoading)
+        {
+            return isLoading ? MessageIntent.Info : MessageIntent.Error;
+        }
+    }
+}
